9a85f5d5bf2d06873a9f8ba1f556a8aaedef1d3e
wilsoncarreno <136414588+wilsoncarreno@users.noreply.github.com>
2025-06-18 10:26:04 -0500

    Retell es una aplicación de escritorio desarrollada en Java con Spring Boot que permite gestionar un sistema de reparaciones y ventas de llantas. Su objetivo es llevar el registro de servicios prestados, ventas realizadas y mejorar la organización interna de un taller o establecimiento dedicado al mantenimiento y comercialización de llantas

---
+++ src/main/java/com/retell/retell/RetellApplication.java
@@ -0,0 +1,13 @@
+package com.retell.retell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retell/retell/RetellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retell.retell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
